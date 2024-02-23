d2128ff40ac2dd5b2def1c509af87a29981647fa
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-23 14:51:47 +0530

    implemetnted a route to handle admin request

--- app.js
+++ app.js
@@ -11,6 +11,32 @@ app.get("/", (req, res) => {
   });
 });
 
+const username = "admin";
+const password = "password";
+
+app.get("/admin", (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).send({
+            success: false,
+            message:"username/password is required"
+        })
+    }
+
+  const checkAuthorize = matchCredentials(username, password);
+
+  if (!checkAuthorize) {
+    res.status(400).send({
+      success: false,
+      messsage: "you are not authorized to access this",
+    });
+  } else {
+    res.status(200).send({
+      success: true,
+      messsage: `welcome ${username}`,
+    });
+  }
+});
 app.listen(PORT, () => {
   console.log(`server is listening on ${PORT}`);
 });
