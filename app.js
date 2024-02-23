f57f8c9ceb90a4be1268e9ed5f94678a604d6307
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-23 14:52:58 +0530

    mounted express.json midllreware to parse incoming request

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 const PORT = 3000;
-
+app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).send({
     success: true,
