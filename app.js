194efea783a0faa434881c5dcfb3b92b59b39da9
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-18 23:26:27 +0530

    added fs logs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 let users = [];
-// console.log(fs);
+console.log(fs);
 try {
   let userData = fs.readFileSync("users.json", "utf-8");
 
