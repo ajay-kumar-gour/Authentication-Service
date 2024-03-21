7625a6c3467c3746855ed6a194a26d7d07e54d16
ajay-kumar-gour <me.akgour@gmail.com>
2024-03-21 23:47:35 +0530

    updated app .js

--- app.js
+++ app.js
@@ -44,10 +44,9 @@ const adminchecker = (req, res, next) => {
     });
   }
 };
+
 const matchCredentials = (req, res, next) => {
   try {
-    // const user = "admin";
-    // const pwd = "pwd";
     const { username, password } = req.body;
 
     if (!username || !password) {
@@ -56,23 +55,15 @@ const matchCredentials = (req, res, next) => {
         message: "username/password is required",
       });
     }
-    // if (username !== user || password !== pwd) {
-    //   return res
-    //     .status(400)
-    //     .send({ success: false, message: "Invalid credentials" });
-    // } else {
-    //   next();
-    // }
 
-    const user = users.find((user) => {
-      return user.username === username;
-    });
-    console.log(user);
+    const user = users.find((user) => user.username === username);
+
     if (!user) {
       return res
         .status(401)
         .send({ success: false, message: "user does not exist" });
     }
+
     if (user.password !== password) {
       return res
         .status(401)
@@ -86,10 +77,6 @@ const matchCredentials = (req, res, next) => {
   }
 };
 
-// function matchCredentials(user, pwd) {
-//   if (user === username && pwd === password) return true;
-//   return false;
-// }
 app.get("/admin", matchCredentials, (req, res) => {
   const { username } = req.body;
   res.status(200).send({
@@ -106,22 +93,17 @@ app.post("/user", (req, res) => {
         .status(400)
         .send({ success: false, message: "username/password is missing" });
     }
-    const existingUser = users.find((user) => {
-      return user.username === username;
-    });
+    const existingUser = users.find((user) => user.username === username);
 
     if (existingUser) {
       return res
         .status(409)
-        .send({ success: false, message: "user already exist" });
+        .send({ success: false, message: "user already exists" });
     }
-    // create a new suer objet
 
     const newUser = { username, password };
 
-    users.push(newUser); // this is in uers array
-
-    //now update the new users array to the json file
+    users.push(newUser);
 
     fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
 
@@ -135,7 +117,7 @@ app.post("/user", (req, res) => {
 });
 
 app.get("/users", adminchecker, (req, res) => {
-  if (users.length == 0) {
+  if (users.length === 0) {
     res.status(400).send({
       success: true,
       message: "no users found",
@@ -143,8 +125,8 @@ app.get("/users", adminchecker, (req, res) => {
   } else {
     res.status(200).send({
       success: true,
-      mesaage: "users fetched",
-      totatUsers: users.length,
+      message: "users fetched",
+      totalUsers: users.length,
       users,
     });
   }
@@ -152,22 +134,17 @@ app.get("/users", adminchecker, (req, res) => {
 
 app.delete("/user", (req, res) => {
   const username = req.body.username;
-  console.log(username);
   if (!username) {
     return res.status(400).send({
       success: false,
-      mesaage: "username is required",
+      message: "username is required",
     });
   }
 
-  const index = users.findIndex((user) => {
-    return user.username === username;
-  });
+  const index = users.findIndex((user) => user.username === username);
 
-  console.log("indexofuser", index);
-
-  if (index != -1) {
-    const updatedUsers = users.splice(index, 1);
+  if (index !== -1) {
+    users.splice(index, 1);
 
     fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
 
@@ -189,38 +166,39 @@ app.put("/user", (req, res) => {
   const { username, oldPassword, newPassword } = req.body;
   if (!username || !oldPassword || !newPassword) {
     return res.status(400).send({
-      succcess: false,
-      message: "username/oldpassword/newpassowrd is required",
+      success: false,
+      message: "username/oldpassword/newpassword is required",
     });
   }
 
-  const existingUser = users.find((user) => {
-    return user.username === username;
-  });
+  const existingUser = users.find((user) => user.username === username);
 
   if (!existingUser) {
     return res.status(400).send({
-      succes: false,
-      mesaage: "user does not exist",
+      success: false,
+      message: "user does not exist",
     });
   }
-  if (existingUser.password != oldPassword) {
+
+  if (existingUser.password !== oldPassword) {
     return res.status(400).send({
-      succes: false,
-      message: "old password does not matched",
+      success: false,
+      message: "old password does not match",
     });
   }
+
   existingUser.password = newPassword;
 
   fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
 
   res.status(200).send({
     success: true,
-    message: `user ${username} passwords were updated successfully`,
+    message: `user ${username} password updated successfully`,
     userNewDetail: existingUser,
     users,
   });
 });
+
 app.listen(PORT, () => {
   console.log(`server is listening on ${PORT}`);
 });
